14eec2699d139683a428e803f60bec825c470890
Alvin J. Alexander <alvinalexander@gmail.com>
2022-08-09 21:41:27 +0700

    up

--- src/loan-nft.ts
+++ src/loan-nft.ts
@@ -1,71 +1,134 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  LoanNFT,
   CovenantAccept,
   CovenantLiquidate,
   CovenantListed,
   CovenantReturnNFT,
   CovenantUnlisted,
-  OwnershipTransferred
 } from "../generated/LoanNFT/LoanNFT"
-import { ExampleEntity } from "../generated/schema"
+import { Event, Covenant } from "../generated/schema"
 
 export function handleCovenantAccept(event: CovenantAccept): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let entryId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())
+  let covenant = Covenant.load(entryId) 
+  
+  if (covenant != null) {
+    covenant.timestamp = event.block.timestamp
+    covenant.borrowedAt = event.block.timestamp
+    covenant.daysBorrow = event.params.daysToRent
+    covenant.status = "LOCKED"
+    covenant.borrower = event.params.borrower
+    covenant.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.itemId = event.params.itemId
-  entity.borrower = event.params.borrower
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ERC721INTERFACE(...)
-  // - contract.PERCENTS_DIVIDER(...)
-  // - contract.currentItemId(...)
-  // - contract.feePerThousand(...)
-  // - contract.feeReceiver(...)
-  // - contract.idToCovenant(...)
-  // - contract.loanAccepted(...)
-  // - contract.loanLiquidated(...)
-  // - contract.owner(...)
+  // register  event
+  let eventId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.block.timestamp.toString())
+  let eventItem = new Event(eventId)
+  eventItem.timestamp = event.block.timestamp	
+  eventItem.nftContract = event.params.nftContract
+  eventItem.tokenId = event.params.tokenId
+  eventItem.action = 'LoanAccept'
+  eventItem.actionAddress = event.params.borrower
+  eventItem.params = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())		 	
+  eventItem.save()
 }
 
-export function handleCovenantLiquidate(event: CovenantLiquidate): void {}
+export function handleCovenantLiquidate(event: CovenantLiquidate): void {
+  let entryId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())
+  let covenant = Covenant.load(entryId) 
+  
+  if (covenant != null) {
+      covenant.timestamp = event.block.timestamp
+      covenant.status = "LIQUIDATE"
+      covenant.save()
+  }
+  // register  event
+  let eventId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.block.timestamp.toString())
+  let eventItem = new Event(eventId)
+  eventItem.timestamp = event.block.timestamp	
+  eventItem.nftContract = event.params.nftContract
+  eventItem.tokenId = event.params.tokenId
+  eventItem.action = 'Liquidate'
+  eventItem.actionAddress = event.block.author;
+  eventItem.params = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())		 	
+  eventItem.save()
+}
 
-export function handleCovenantListed(event: CovenantListed): void {}
+export function handleCovenantListed(event: CovenantListed): void {
+  let entryId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())
+  let covenant = Covenant.load(entryId);
+  if (covenant == null) {
+      covenant = new Covenant(entryId)
+  }
+  covenant.timestamp = event.block.timestamp
+  covenant.txhash = event.transaction.hash.toHexString()
+  covenant.logIndex = event.transactionLogIndex
 
-export function handleCovenantReturnNFT(event: CovenantReturnNFT): void {}
+  covenant.nftContract = event.params.nftContract
+  covenant.tokenId = event.params.tokenId
+  covenant.itemId = event.params.itemId
+  covenant.lender = event.params.lender
+  covenant.borrower = event.params.borrower
+  covenant.tokenAdr = event.params.tokenAdr
+  covenant.price = event.params.price
+  covenant.priceBorrow = event.params.priceBorrow
+  covenant.maxDays = event.params.maxDays
+  covenant.status = "LISTING"
+  covenant.daysBorrow = BigInt.fromI32(0)
+  covenant.borrowedAt = BigInt.fromI32(0)
+  covenant.save()
 
-export function handleCovenantUnlisted(event: CovenantUnlisted): void {}
+  // register  event
+  let eventId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.block.timestamp.toString())
+  let eventItem = new Event(eventId)
+  eventItem.timestamp = event.block.timestamp	
+  eventItem.nftContract = event.params.nftContract
+  eventItem.tokenId = event.params.tokenId
+  eventItem.action = 'LoanCreated'
+  eventItem.actionAddress = event.params.lender
+  eventItem.params = event.params.tokenAdr.toHex().concat('-').concat(event.params.price.toString()).concat('-').concat(event.params.priceBorrow.toString())		 	
+  eventItem.save()
+    
+  
+}
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+export function handleCovenantReturnNFT(event: CovenantReturnNFT): void {
+  let entryId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())
+  let covenant = Covenant.load(entryId) 
+  
+  if (covenant != null) {
+      covenant.timestamp = event.block.timestamp
+      covenant.status = "RETURN_NFT"
+      covenant.save()
+  }
+  // register  event
+  let eventId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.block.timestamp.toString())
+  let eventItem = new Event(eventId)
+  eventItem.timestamp = event.block.timestamp	
+  eventItem.nftContract = event.params.nftContract
+  eventItem.tokenId = event.params.tokenId
+  eventItem.action = 'ReturnNFT'
+  eventItem.actionAddress = event.block.author;
+  eventItem.params = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())		 	
+  eventItem.save()
+}
+
+export function handleCovenantUnlisted(event: CovenantUnlisted): void {
+  let entryId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())
+  let covenant = Covenant.load(entryId) 
+  
+  if (covenant != null) {
+      covenant.timestamp = event.block.timestamp
+      covenant.status = "UNLIST"
+      covenant.save()
+  }
+  // register  event
+  let eventId = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.block.timestamp.toString())
+  let eventItem = new Event(eventId)
+  eventItem.timestamp = event.block.timestamp	
+  eventItem.nftContract = event.params.nftContract
+  eventItem.tokenId = event.params.tokenId
+  eventItem.action = 'UnList'
+  eventItem.actionAddress = event.block.author;
+  eventItem.params = event.params.nftContract.toHex().concat('-').concat(event.params.tokenId.toString()).concat('-').concat(event.params.itemId.toString())		 	
+  eventItem.save()
+}
